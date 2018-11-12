8b5e6dad4147e77f4acfb4c87005ab359e9f099e
Sarah Dahnke <sarahrosedahnke@gmail.com>
2018-11-12 16:45:14 -0500

    adding day19

---
+++ 19_nov12_day19/introtojs/script.js
@@ -0,0 +1,87 @@
+//this is a Javascript comment
+
+console.log('Hiiiiiiiiiiii');
+
+
+//define a variable
+//what is a variable?
+//representing a piece of data
+//assign a value to it
+
+//ES5 defines variables with keyword var
+
+//ES6 defines variables with two keywords: let and const
+
+//string
+var firstName = 'Sarah'
+var lastName = 'Dahnke'
+
+//console.log(firstName[4]);
+
+//console.log(firstName);
+
+//console.log(firstName, lastName);
+
+//concatenate
+//console.log(firstName + lastName);
+
+//integers
+var num1 = 5
+
+//floats
+var num2 = 55.6
+
+var sum = num1 + num2 
+
+//console.log(num1 + num2);
+//console.log(sum);
+
+//this is another form of string concatenation
+//console.log('5' + '55.6');
+
+//define an array
+
+//can store a lot of data
+//can represent a matrix
+//represents a list
+
+var firstArray = ['bert', 'ernie', 'big bird', 'snuffie']
+
+var secondArray = ['kermit', 'miss piggy', 'gonzo', 'swedish chef']
+
+//console.log(firstArray);
+
+//indexing the array
+//console.log(firstArray[2]);
+
+//console.log(secondArray);
+
+//making a multidimensional array
+
+var thirdArray = [firstArray, secondArray]
+
+console.log(thirdArray);
+
+//getting to 'gonzo' from thirdArray
+console.log(thirdArray[1][2]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
